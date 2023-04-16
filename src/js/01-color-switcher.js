be94a652d4eb035980a9da76bf436042610f23c3
Olha Tverdokhlib <balabolya@gmail.com>
2023-04-16 23:40:04 +0200

    hw-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,46 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+btnStart.disabled = false;
+btnStop.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function updateBackgroundColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function togle() {
+  if (btnStart.disabled) {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    return;
+  } else btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function onBtnStartClick() {
+    timerId = setInterval(updateBackgroundColor, 1000);
+     if (btnStart.disabled) {
+       btnStart.disabled = false;
+       btnStop.disabled = true;
+       return;
+     } else btnStart.disabled = true;
+     btnStop.disabled = false;
+   
+}
+
+function onBtnStopClick() {
+    clearInterval(timerId);
+      btnStart.disabled = false;
+      btnStop.disabled = true;
+      return;
+ 
+};
